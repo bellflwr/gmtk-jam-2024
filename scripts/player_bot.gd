1157e2e95f4e8e430a9fa4a7b2d57ea532650e0b
juicy-croissant <benjamindent97@gmail.com>
2024-08-19 21:12:52 -0400

    ye
    ye

--- scripts/player_bot.gd
+++ scripts/player_bot.gd
@@ -1,13 +1,21 @@
 extends CharacterBody2D
 
-@onready var animation = $AnimatedSprite2D
+@onready var treads = $Treads
+@onready var body = $body
+@onready var arm_r = $right_arm
+@onready var arm_l = $left_arm
+@onready var head = $head
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+func _ready():
+	treads.play()
+	arm_r.play()
+	arm_l.play()
+
 func _process(delta):
 	var mouse_position = get_global_mouse_position()
-	animation.play()
 	var direction = Vector2.ZERO
 	if Input.is_action_pressed("move_up"):
 		direction += Vector2.UP
@@ -20,10 +28,14 @@ func _process(delta):
 	
 	if direction != Vector2.ZERO:
 		velocity = velocity.move_toward((direction.normalized() * 100), (delta * 800))
-		animation.set_animation("walk")
+		treads.set_animation("move")
+		arm_r.set_animation("move")
+		arm_l.set_animation("move")
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, (delta * 1200))
-		animation.set_animation("idle")
+		treads.set_animation("default")
+		arm_r.set_animation("default")
+		arm_l.set_animation("default")
 	print(velocity)
 	
 	look_at(mouse_position)
